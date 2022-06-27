87402d61e213d2c30a6180faf53166942af05c43
Michael Law <rmdashrfv@hey.com>
2022-06-27 17:03:48 -0400

    added starter pokemon

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 const pokemon = ['charmander', 'bulbasaur', 'squirtle']
+const pokemonIDs = ['001', '004', '007']
+
+// DOM = Document Object Model
+// loop over every ID
+// create an HTML element
+// set the HTML element values
+// append HTML element to the DOM
+
+pokemonIDs.map((id) => {
+  let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
+  console.log('img url', imgUrl)
+  let img = document.createElement('img')
+  img.src = imgUrl
+  img.setAttribute('height', '100px')
+  img.setAttribute('width', '100px')
+  document.body.append(img)
+})
+
 
